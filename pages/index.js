ca946b4d90337cc9a5f43dd50179de9fbeb67982
Hakan Urtimur <dythakanurtimur@gmail.com>
2023-05-19 15:32:17 +0300

    Adding Layout wrapper to _app.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,32 @@
 import React from "react";
-import MeetupList from '../components/meetups/MeetupList'
-import  { img } from '../components/img'
+import MeetupList from "../components/meetups/MeetupList";
+import { img } from "../components/img";
+
 
 export default function HomePage() {
-const DUMMY_MEETUPS = [
-    {id: 'm1', title: 'First meetup', address: 'Some address 3. street.', image: img},
-    {id: 'm2', title: 'First meetup', address: 'Some address 4. street.', image: img},
-    {id: 'm3', title: 'First meetup', address: 'Some address 5. street.', image: img},
-]
-    return <MeetupList meetups = {DUMMY_MEETUPS}  />
+  const DUMMY_MEETUPS = [
+    {
+      id: "m1",
+      title: "First meetup",
+      address: "Some address 3. street.",
+      image: img,
+    },
+    {
+      id: "m2",
+      title: "First meetup",
+      address: "Some address 4. street.",
+      image: img,
+    },
+    {
+      id: "m3",
+      title: "First meetup",
+      address: "Some address 5. street.",
+      image: img,
+    },
+  ];
+  return (
+
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    
+  );
 }
